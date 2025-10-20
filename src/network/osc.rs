5b4512ffd8610e1f422963ed2e012d1d9c54222e
Carsten Heisterkamp <ch@carstenheisterkamp.com>
2025-10-21 00:38:36 +0200

    add osc and websockets

--- src/network/osc.rs
+++ src/network/osc.rs
@@ -26,12 +26,113 @@ impl Plugin for OscReceiverPlugin {
 struct OscMessageReceiver(Receiver<OscMessage>);
 
 fn start_osc_listener_thread(listen_address: String, sender: Sender<OscMessage>) {
-    // TODO: 'start_osc_listener_thread' hier einfügen
+    thread::spawn(move || {
+        let addr = SocketAddrV4::from_str(&listen_address)
+            .expect("Failed to parse OSC listen address");
+        let socket = UdpSocket::bind(addr).expect("Failed to bind OSC socket");
+
+        info!("OSC receiver listening on {}", listen_address);
+
+        let mut buf = [0u8; 65536];
+
+        loop {
+            match socket.recv_from(&mut buf) {
+                Ok((size, _addr)) => {
+                    let packet = rosc::decoder::decode_udp(&buf[..size]);
+                    match packet {
+                        Ok((_, OscPacket::Message(msg))) => {
+                            if sender.send(msg).is_err() {
+                                error!("Failed to send OSC message to channel");
+                                break;
+                            }
+                        }
+                        Ok((_, OscPacket::Bundle(bundle))) => {
+                            for packet in bundle.content {
+                                if let OscPacket::Message(msg) = packet {
+                                    if sender.send(msg).is_err() {
+                                        error!("Failed to send OSC message to channel");
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            error!("Failed to decode OSC packet: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("Failed to receive OSC packet: {}", e);
+                }
+            }
+        }
+    });
 }
 
 fn process_osc_messages_system(
     receiver: Res<OscMessageReceiver>,
-    mut event_writer: EventWriter<GameEvent>,
+    mut event_writer: MessageWriter<GameEvent>,
 ) {
-    // TODO: 'process_osc_messages_system
+    while let Ok(msg) = receiver.0.try_recv() {
+        match msg.addr.as_str() {
+            "/hand/count" => {
+                if let Some(OscType::Int(count)) = msg.args.first() {
+                    event_writer.write(GameEvent::HandCountChanged(*count));
+                }
+            }
+            "/hand/gesture" => {
+                if let (Some(OscType::Int(hand_id)), Some(gesture)) =
+                    (msg.args.get(0), msg.args.get(1))
+                {
+                    if let OscType::String(gesture_str) = gesture {
+                        event_writer.write(GameEvent::HandGesture {
+                            hand_id: *hand_id,
+                            gesture: gesture_str.clone(),
+                        });
+                    }
+                }
+            }
+            "/hand/pinch" => {
+                if let (Some(OscType::Int(hand_id)), Some(distance)) =
+                    (msg.args.get(0), msg.args.get(1))
+                {
+                    let dist_f32 = match distance {
+                        OscType::Float(f) => *f,
+                        OscType::Double(d) => *d as f32,
+                        _ => continue,
+                    };
+                    event_writer.write(GameEvent::HandPinchDistance {
+                        hand_id: *hand_id,
+                        distance: dist_f32,
+                    });
+                }
+            }
+            "/object/detected" => {
+                if let (Some(name), Some(x), Some(y)) =
+                    (msg.args.get(0), msg.args.get(1), msg.args.get(2))
+                {
+                    if let OscType::String(name_str) = name {
+                        let x_f32 = match x {
+                            OscType::Float(f) => *f,
+                            OscType::Double(d) => *d as f32,
+                            _ => continue,
+                        };
+                        let y_f32 = match y {
+                            OscType::Float(f) => *f,
+                            OscType::Double(d) => *d as f32,
+                            _ => continue,
+                        };
+                        event_writer.write(GameEvent::ObjectDetected {
+                            name: name_str.clone(),
+                            x: x_f32,
+                            y: y_f32,
+                        });
+                    }
+                }
+            }
+            _ => {
+                warn!("Unknown OSC address: {}", msg.addr);
+            }
+        }
+    }
 }
